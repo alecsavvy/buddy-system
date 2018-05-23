6549dca35e75d6f2dd5d6019d931b6fdcb1aa164
Alec Savoy <alecsavvy@Alecs-MacBook-Pro.local>
2018-05-23 11:45:17 -0700

    added user ids

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+const uuid = require('uuid/v1')
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(process.cwd(), 'public', 'index.html');
@@ -11,16 +12,37 @@ const server = express()
 
 const io = socketIO(server);
 
-var users = {}
-counter = 0
+var users = {};
 
 io.on('connection', (socket) => {
-    userkey = Date.now();
-    users[userkey] = counter;
-    counter += 1;
-    var log = `Client `+ counter +` connected`
-    console.log(log);
-    socket.on('disconnect', () => console.log('Client' + users[userkey] +' disconnected'));
+    userid = uuid();
+    loginUser(userid); // User logged in.
+    socket.emit('user', userid);
+    socket.on('loc', function(location){
+        users[location.id]['currentLocation'] = {
+            latitude: location.lat,
+            longitude: location.lng
+        }
+        console.log('User: ' + location.id);
+        console.log('at: ' + users[location.id]['currentLocation']['latitude'] + ', ' + users[location.id]['currentLocation']['longitude'])
+    });
+    socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
+function loginUser(id){
+    // Here would be a function that would validate
+    // UO id and password before allowing access.
+    // Ommitting for demonstration purposes.
+    users[id] = {
+        'currentLocation': {
+          'latitude': "None set",
+          'longitude': "None set"  
+        },
+        'destination': {
+            'latitude': "None set",
+            'longitude': "None set"
+        }
+    };
+}
 
 setInterval(() => io.emit('time', new Date().toString()), 1000);
